2381c7c390f87c0d75c2c6842462e169e0cac1a1
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-06-12 01:00:19 +0300

    style:add profile save method

--- iReview/models.py
+++ iReview/models.py
@@ -1,12 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
 
 class Profile(models.Model):
-    profile_photo = models.ImageField(null=True)
-    bio = models.CharField(max_length=200, blank=True)
+    profile_photo = models.ImageField(upload_to='media')
+    bio = models.TextField()
     projects = models.ForeignKey()
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
+
+    def __str__(self):
+        return self.bio
+
+    def save_profile(self):
+        self.save()   
 
 
 class Rating(models.Model):
