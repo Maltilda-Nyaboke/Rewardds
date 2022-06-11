25ede5d0312a25dd0215184ebfe5d30daad812c3
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-06-12 00:54:50 +0300

    style:create profile model

--- iReview/models.py
+++ iReview/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    profile_photo = models.ImageField(null=True)
+    bio = models.CharField(max_length=200, blank=True)
+    projects = models.ForeignKey()
