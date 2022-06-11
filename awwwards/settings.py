65570f76f6317e54f52b87f70fcafd898166e235
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-06-11 21:00:19 +0300

    style:add login auth

--- awwwards/settings.py
+++ awwwards/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_URL = '/login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/register'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
