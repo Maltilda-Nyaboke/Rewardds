61ec5af591a41c55025e9f756918162ff86558d7
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-06-12 00:56:33 +0300

    style:add rating model

--- iReview/models.py
+++ iReview/models.py
@@ -7,3 +7,7 @@ class Profile(models.Model):
     profile_photo = models.ImageField(null=True)
     bio = models.CharField(max_length=200, blank=True)
     projects = models.ForeignKey()
+
+
+class Rating(models.Model):
+    name = models.CharField(max_length=30)    
