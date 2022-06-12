8d2f68e32367232f4c7a8c7719c118794ab8ead9
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-06-12 10:15:24 +0300

    style:add project form

--- iReview/forms.py
+++ iReview/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth import login,authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import *
 
 
 
@@ -11,3 +12,9 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1', 'password2',]
+
+class AddProjectForm():
+    model = Project
+    class Meta:
+        fields = ['title','description','url']
+
