e011f5ed1e979840158ea31e49e0b0f73232dba0
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-06-13 21:30:09 +0300

    bug:migrations

--- iReview/models.py
+++ iReview/models.py
@@ -9,8 +9,8 @@ from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
-    profile_photo = models.ImageField(upload_to='media')
-    bio = models.TextField()
+    profile_photo = models.ImageField(upload_to='media',null= True)
+    bio = models.TextField(null=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
     contact = models.CharField(max_length=50, blank=True, null=True)
 
@@ -24,24 +24,12 @@ class Profile(models.Model):
         self.delete()
         
 
-class Rating(models.Model):
-    design = models.IntegerField(default=0,validators=[MinValueValidator(1),MaxValueValidator(10)])
-    content = models.IntegerField(default=0,validators=[MinValueValidator(1),MaxValueValidator(10)])
-    usability = models.IntegerField(default=0,validators=[MinValueValidator(1),MaxValueValidator(10)])
 
-    def __str__(self):
-        return self.content
-
-    def save_rating(self):
-        self.save() 
-
-    def delete_rating(self):
-        self.delete()        
 
 class Project(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
-    title = models.CharField(max_length=75, blank=False)
-    image = models.ImageField(null= False, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='project')
+    title = models.CharField(max_length=75)
+    image = models.ImageField(upload_to='media',null=False)
     description = models.TextField()
     url = models.URLField()
     posted = models.DateTimeField(auto_now_add=True)
@@ -56,5 +44,22 @@ class Project(models.Model):
     def delete_project(self):
         self.delete()    
 
+
+class Rating(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE,null=False)
+    design = models.IntegerField(default=0,validators=[MinValueValidator(1),MaxValueValidator(10)])
+    content = models.IntegerField(default=0,validators=[MinValueValidator(1),MaxValueValidator(10)])
+    usability = models.IntegerField(default=0,validators=[MinValueValidator(1),MaxValueValidator(10)])
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    average_rate = models.IntegerField(default=0,validators=[MinValueValidator(1),MaxValueValidator(10)])
+
+    def __str__(self):
+        return self.content
+
+    def save_rating(self):
+        self.save() 
+
+    def delete_rating(self):
+        self.delete()        
 
 
