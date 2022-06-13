32807fabd73e89413336ee7ed5f22f1b8feb16af
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-06-14 01:57:27 +0300

    style:add function for project serializer

--- iReview/views.py
+++ iReview/views.py
@@ -96,7 +96,12 @@ def new_project(request):
 
 class ProfileList(APIView):
     def get(self, request, format = None):
-        all_profile = Profile.objects.all()
-        serializers = ProfileSerializer(all_profile,many=True)
+        all_profiles = Profile.objects.all()
+        serializers = ProfileSerializer(all_profiles,many=True)
         return Response(serializers.data)              
 
+class ProjectList(APIView):
+    def get(self, request, format = None):
+        all_projects = Project.objects.all()
+        serializers = ProjectSerializer(all_projects,many=True)
+        return Response(serializers.data)              
